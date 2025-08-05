172a741332dec0fd0b832d83446e6ed858119f48
ManojGitHub1 <manojjivanagi@gmail.com>
2025-08-05 23:16:19 +0530

    signup/signin added

--- oops_proj.py
+++ oops_proj.py
@@ -7,24 +7,41 @@ class chatbook:
         self.menu()
     
     def menu(self):
-        user_input = int(input((""" Welcome to Chatbook!
+        user_input = int(input(("""Welcome to Chatbook!
               1. Signup
               2. Login
               3. Write a post
               4. Message a friend
-              Press any other key to exit.""")))
+              Press any other key to exit.\n""")))
         
-        
-        if user_input == '1':
+        if user_input == 1:
+            self.signup()
+        elif user_input == 2:
             self.login()
-        elif user_input == '2':
-            self.register()
-        elif user_input == '3':
+        elif user_input == 3:
             pass
-        elif user_input == '4':
+        elif user_input == 4:
             pass
         else:
             exit()
+
+    def signup(self):
+        self.username = input("Enter your username: ")
+        self.password = input("Enter your password: ")
+        print(f"User {self.username} signed up successfully!")
+        print("\n")
+        self.menu()
+
+    def login(self):
+        if self.username and self.password:
+            print(f"User {self.username} logged in successfully!\n")
+            self.logged_in = True
+        else:
+            print("Please sign up first.")
+            self.signup()
+        print("\n")
+        self.menu()
+        
 
 
 obj = chatbook()
